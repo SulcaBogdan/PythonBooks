941f9e105bf8776c80ad39978cde76c9900392cd
SulcaBogdan <Stefanbogdansulca@gmail.com>
2023-10-31 20:04:40 +0200

    Explanation added to Problem1480.py

--- LeetCodeProblems/Problem1480.py
+++ LeetCodeProblems/Problem1480.py
@@ -1,5 +1,9 @@
 from typing import List
+"""I began by creating a for loop to iterate through the nums list.
 
+1. Skipped the first element of the list since, regardless of its value, it would be added to its sum with 0.
+2. Replaced nums[i] with the sum of itself and the previous value, i.e., nums[i-1].
+3. Finally, returned the new nums list."""
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
         for i in range(1,len(nums)):
